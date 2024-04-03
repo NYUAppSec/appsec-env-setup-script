dd4bf0bbe8d7f696865c30c617ca515424a4da7c
Robert Todora <roberttodora@gmail.com>
2024-04-03 18:01:49 -0400

    changed script to take arguments instead of prompting user. updated readme to reflect these changes

--- Ubuntu/setup_hw1_ubuntu.sh
+++ Ubuntu/setup_hw1_ubuntu.sh
@@ -1,56 +1,30 @@
 #!/usr/bin/env bash
 
-confirm() {
-    local response
-    while true; do
-        read -p "$1 (y/n): " response
-        case "$response" in
-            [yY][eE][sS]|[yY]) 
-                return 0
-                ;;
-            [nN][oO]|[nN])
-                return 1
-                ;;
-            *)
-                echo "Invalid input. Please enter 'y' for yes or 'n' for no."
-                ;;
-        esac
-    done
-}
+REPO_NAME="$1"
+EMAIL="$2"
+NAME="$3"
+GPG_ID="$4"
 
-while true; do
-    read -p "Enter the name of your repository to clone (make sure your SSH public key has been added to your Github account settings first or this will fail): " REPO_NAME
-    confirm "Your repository name is $REPO_NAME, is this correct?" && break
-done
-
-echo "Cloning repository..."
-git clone git@github.com:NYUAppSec/$REPO_NAME
-
-echo "GPG key setup (make sure you add the key to your github account too!)"
-while true; do
-    read -p "Enter the email address associated with the GitHub account used for this class: " email
-    confirm "Your email is: $email , is this correct?" && break
-done
-
-while true; do
-    read -p "Enter your full name: " name
-    confirm "Your name is: $name , is this correct?" && break
-done
-
-while true; do
-    read -p "Enter your GPG key id (you can find this by running \"gpg --list-secret-keys --keyid-format=long\" in a separate terminal window)" gpg_id
-    confirm "Your GPG key id is: $gpg_id, is this correct?" && break
-done
+if [ -z "$REPO_NAME" ] || [ -z "$EMAIL" ] || [ -z "$NAME" ] || [ -z "$GPG_ID" ]; then
+    echo "Usage: $0 <repo_name> <email> <name> <gpg_id>"
+    exit 1
+fi
 
 echo "Updating package lists..."
 sudo apt-get update
 echo "Installing git..."
 sudo apt install git
 
+echo "Cloning repository..."
+if ! git clone "git@github.com:NYUAppSec/$REPO_NAME"; then
+    echo "Error: Failed to clone repository, make sure you have setup SSH keys on your machine and added the public key to your github account"
+    exit 1
+fi
+
 echo "Adding GPG keys to git config..."
-git config --global user.email "$email"
-git config --global user.name "$name"
-git config --global user.signingkey $gpg_id
+git config --global user.email "$EMAIL"
+git config --global user.name "$NAME"
+git config --global user.signingkey "$GPG_ID"
 git config --global commit.gpgsign true
 
 echo "Installing lcov..."
