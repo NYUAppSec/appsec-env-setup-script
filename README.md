2b4ccae19e58575035f78b663693a6a64b51e732
Robert Todora <roberttodora@gmail.com>
2024-04-03 17:02:57 -0400

    added hw2 setup script, changed file names, edited README and added more clarity, and tweaked github action to have new filenames

--- README.md
+++ README.md
@@ -2,23 +2,33 @@
 Setup script for HW 1-3 of NYU's application security class on a freshly installed ubuntu VM
 
 ## Usage
-Run this script from the directory where you want your homework repo to be cloned into
+Run this script from the location where you want your homework repo to be cloned into
 
-`chmod +x setup.sh`
+Helpful tips for usage: 
+Make sure you create the GPG and SSH keys beforehand if you do not have them already 
 
-`./setup.sh`
+[Generating a new SSH key](https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent)
 
-## Assumptions about setup
+[Generating a new GPG Key](https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key)
+
+The HW1 setup script will ask for the GPG key ID, this can be found by running 
+
+`gpg --list-secret-keys --keyid-format=long`
+ 
+ See the link above for more information about the GPG key ID
+
+
+```
+chmod +x setup.sh
+./setup.sh
+```
+
+### Assumptions about setup
 
 - GPG key and SSH key already exist
 
-### Separate folders containing scripts for different OS 
-- Ubuntu
-- Debian 
-- Mac 
-
 ### HW 1  
-- Add keys to git config
+- Add gpg key to git config, enable automatic signing on commit
 - git clone repo by name  
 - install AFL++ 
 - install GDB 
--- Ubuntu/setup_hw1_ubuntu.sh
+++ Ubuntu/setup_hw1_ubuntu.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Function to prompt for confirmation
 confirm() {
     local response
     while true; do
@@ -19,44 +18,30 @@ confirm() {
     done
 }
 
-# Prompt user for email address
+while true; do
+    read -p "Enter the name of your repository to clone (make sure your SSH public key has been added to your Github account settings first or this will fail): " REPO_NAME
+    confirm "Your repository name is $REPO_NAME, is this correct?" && break
+done
+
+echo "Cloning repository..."
+git clone git@github.com:NYUAppSec/$REPO_NAME
+
+echo "GPG key setup (make sure you add the key to your github account too!)"
 while true; do
     read -p "Enter the email address associated with the GitHub account used for this class: " email
     confirm "Your email is: $email , is this correct?" && break
 done
 
-# Prompt user for full name
 while true; do
     read -p "Enter your full name: " name
     confirm "Your name is: $name , is this correct?" && break
 done
 
-
-# read -p "Enter the email address associated with your github account used for HW1: " email
-# read -p "Your email is: $email , is this correct? (y/n):"
-
-# read -p "Enter your full name: " name
-# read -p "Your name is: $name , is this correct? (y/n):" 
-# read -p "Enter a name for your key to store in the ~/.ssh directory (Leave empty for \"id_rsa\"): " key_name
-
-## Set default key name if empty
-# if [ -z "$key_name" ]; then
-#     key_name="id_rsa"
-# fi
-# KEY_PATH="$HOME/.ssh/$key_name"
-# echo "Key path is: $KEY_PATH"
-
-# echo "Generating SSH key..."
-# ssh-keygen -t rsa -b 4096 -f $KEY_PATH
-
-## Generate GPG key
-# echo "Generating GPG key..."
-# gpg --full-generate-key
 while true; do
     read -p "Enter your GPG key id (you can find this by running \"gpg --list-secret-keys --keyid-format=long\" in a separate terminal window)" gpg_id
     confirm "Your GPG key id is: $gpg_id, is this correct?" && break
 done
-## Add SSH and GPG keys to git config
+
 echo "Updating package lists..."
 sudo apt-get update
 echo "Installing git..."
@@ -68,14 +53,6 @@ git config --global user.name "$name"
 git config --global user.signingkey $gpg_id
 git config --global commit.gpgsign true
 
-while true; do
-    read -p "Enter the name of your repository to clone (make sure your SSH public key has been added to your Github account settings first or this will fail): " REPO_URL
-    confirm "Your repository name is $REPO_URL, is this correct?" && break
-done
-
-echo "Cloning repository..."
-git clone git@github.com:NYUAppSec/$REPO_URL
-
 echo "Installing lcov..."
 sudo apt-get install lcov
 
@@ -86,5 +63,3 @@ echo "Installing GDB..."
 sudo apt-get install gdb
 
 echo "Setup for HW1 complete..."
-
-# read -p "Start HW2 setup?" hw2_response
