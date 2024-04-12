a5ddaaea29ae7fc9180bdc23580ccb1752eb1fdb
Robert Todora <roberttodora@gmail.com>
2024-04-12 12:07:07 -0400

    renamed workflow files and fixed path location of android studio

--- Linux/setup_hw4.sh
+++ Linux/setup_hw4.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -ex 
 
+sudo apt update
 # https://ubuntu.com/tutorials/install-jre#2-installing-openjdk-jre
-sudo apt install default-jre
+sudo apt -y install default-jre
 java -version 
 
 sudo add-apt-repository ppa:maarten-fonville/android-studio
 sudo apt update
-sudo apt install android-studio
+sudo apt -y install android-studio
 
-cd /usr/local/android-studio/bin
+cd /opt/android-studio/bin
 ./studio.sh
 
-echo "Setup for HW4 complete, you can now launch the android studio application and continue with the homework steps ..."
+echo "Setup for HW4 complete..."
