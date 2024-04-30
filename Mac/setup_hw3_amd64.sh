4a5a02cea52d3d8e3f0d99e4296ab4c749407bca
Robert Todora <roberttodora@gmail.com>
2024-04-30 14:04:18 -0400

    removed validation of binary to hw3 on mac

--- Mac/setup_hw3_amd64.sh
+++ Mac/setup_hw3_amd64.sh
@@ -13,19 +13,12 @@ open /Applications/Docker.app
 # Kubectl installation from https://kubernetes.io/docs/tasks/tools/install-kubectl-macos/
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"
 
-# Validate binary
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl.sha256"   
-
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-
 chmod +x ./kubectl
 
 sudo mv ./kubectl /usr/local/bin/kubectl
 sudo chown root: /usr/local/bin/kubectl
 
 kubectl version --client
-
-rm kubectl.sha256
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
 sudo install minikube-darwin-amd64 /usr/local/bin/minikube
